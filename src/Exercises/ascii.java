ba23dc56bf97992fe6b45d978bed7564cba97fdd
Fredy Lopez <fredygutierrez1661@gmail.com>
2026-01-12 22:15:27 -0600

    Clase creada para la utilziacion de variables y constantes

---
+++ src/Exercises/ascii.java
@@ -0,0 +1,14 @@
+package Exercises;
+
+public class ascii {
+    public static void main(String[] args) {
+        System.out.println("  ******");
+        System.out.println("*         *");
+        System.out.println("*  O  O   *" );
+        System.out.println("*    ^    *");
+        System.out.println("*         * ");
+        System.out.println("* \\_____/ * ");
+        System.out.println("  *******");
+
+    }
+}
---
+++ src/VariablesAndConstants.java
@@ -0,0 +1,13 @@
+public class VariablesAndConstants {
+    public static void main(String[] args) {
+        String name = "Fredy";
+        int age = 25;
+        System.out.println("Tu nombre es " + name + " Y tu edad es: " + age);
+        
+        //Constantes
+
+        final String email = "fredygut@gmail.com";
+        email = "dora@gmail.com";
+        System.out.println(email);
+    }
+}
