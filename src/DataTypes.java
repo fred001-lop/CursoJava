102c9c3b94f53897ac12557dc47690789b4c516e
Fredy Lopez <fredygutierrez1661@gmail.com>
2026-01-12 23:08:16 -0600

    Datos agregados a las class

---
+++ src/DataTypes.java
@@ -0,0 +1,18 @@
+public class DataTypes {
+    public static void main(String[] args) {
+        //Tipos de datos primitivos
+
+        int myInt = 25;
+
+        double myDouble = 1.77;
+        //Hay float, long, byte
+        char myChar = 'a';
+
+        boolean myBoolean = true;
+
+        String myString = "Iniciando con JAVA";
+        System.out.println(myString.getClass().getSimpleName());
+
+
+    }
+}
--- src/VariablesAndConstants.java
+++ src/VariablesAndConstants.java
@@ -3,11 +3,11 @@ public class VariablesAndConstants {
         String name = "Fredy";
         int age = 25;
         System.out.println("Tu nombre es " + name + " Y tu edad es: " + age);
-        
+
         //Constantes
 
         final String email = "fredygut@gmail.com";
-        email = "dora@gmail.com";
+//        email = "dora@gmail.com";
         System.out.println(email);
     }
 }
