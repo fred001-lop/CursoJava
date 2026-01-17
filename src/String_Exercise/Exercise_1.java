a70e7aca03b726552feffe832e0370256ed2fe9b
Fredy Lopez <fredygutierrez1661@gmail.com>
2026-01-16 22:27:32 -0600

    Realizacion de ejercicios del curso

--- src/String_Exercise/Exercise_1.java
+++ src/String_Exercise/Exercise_1.java
@@ -2,6 +2,63 @@ package String_Exercise;
 
 public class Exercise_1 {
     public static void main(String[] args) {
+        //1-- Concatena dos cadenas
+        var cadena1 = "Fredy";
+//        var cadena2 = 26;
+//
+//        System.out.println(cadena1 + cadena2);
+
+        //2-- Muestra la longitud de una cadena de texto
+
+//        var longitud = "Mostra la longitud de una cadena";
+//        System.out.println(longitud.length());
+
+        //3-- Muestra el primer y ultimo caracter de un string
+//        String name = "Fredy";
+//        System.out.println(name.charAt(0));
+//        System.out.println(name.charAt(4));
+
+        //4-- Convierte a mayusculas y minusculas un string
+//        String miniscu = "minusculas";
+//        String mayusCulas = "MAYUSCULAS";
+//
+//        System.out.println(miniscu.toUpperCase());
+//        System.out.println(mayusCulas.toLowerCase());
+
+        //5-- Comprueba si una cadena de texto contiene una palabra en concreto
+
+//        String palabra = "Concreto";
+//        System.out.println(palabra.contains("C"));
+
+        //6-- Formatea un string con un entero
+//        var age = 26;
+//        System.out.println(String.format("Hola %s. Tengo %d", cadena1, age));
+
+
+        //7-- ELimina los espacios en blanco al principio y final
+
+//        System.out.println(" ELimina los espacios en blanco al principio y final ".trim());
+
+        //8-- Sustituye todos los espacios en blanco de un string por un guion (-)
+
+//        String reem = "Reemplazar los espaccios en blanco con un guion";
+//        System.out.println(reem.replace(" ", "-"));
+
+        //9-- Comprueba si dos strings son iguales
+//        String compa = "Hola";
+//        String comp2 = "Hoa";
+//        System.out.println(compa == comp2);
+
+
+        //10-- Comprueba si dos strings tienen la misma longitud
+
+        String pruebaLongitud = "Prueba";
+        String pruebaLongitud2 = "Preeba";
+
+        System.out.println(pruebaLongitud.length() == pruebaLongitud2.length());
+
+
+
 
     }
 }
