c6b8f40f16c835ec34246b801595c88cda984534
Fredy Lopez <fredygutierrez1661@gmail.com>
2026-01-22 22:20:06 -0600

    Nuevo archivo usando un ciclo for

---
+++ src/Loops/loops_1.java
@@ -0,0 +1,10 @@
+package Loops;
+
+public class loops_1 {
+    public static void main(String[] args) {
+        //For
+        for (int i = 0; i < 5; i++){
+            System.out.println("Hola, Java!");
+        }
+    }
+}
