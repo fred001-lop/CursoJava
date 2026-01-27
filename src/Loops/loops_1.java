ee31763d3bcd7c0cb13406cd8f62105ff5e2eb44
Fredy Lopez <fredygutierrez1661@gmail.com>
2026-01-26 21:37:23 -0600

    Trabajando con for-each

--- src/Loops/loops_1.java
+++ src/Loops/loops_1.java
@@ -1,10 +1,33 @@
 package Loops;
 
+import java.util.HashSet;
+
 public class loops_1 {
     public static void main(String[] args) {
         //For
         for (int i = 0; i < 5; i++){
             System.out.println("Hola, Java!");
+        }
+
+        String [] names = {"Fredy","Gerardo", "Gutierrez"};
+        for(int index = 0; index < names.length; index++){
+
+            System.out.println(names[index]);
+        }
+
+        //For-each
+        for (String name:names){
+            System.out.println(name);
+        }
+        HashSet<Integer> numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+        numbers.add(4);
+        numbers.add(5);
+
+        for (Integer numero: numbers){
+            System.out.println(numero);
         }
     }
 }
