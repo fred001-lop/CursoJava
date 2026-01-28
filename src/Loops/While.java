f28d0a18d0754c8d31420265c1726e610dc2a1a0
Fredy Lopez <fredygutierrez1661@gmail.com>
2026-01-27 19:56:34 -0600

    Trabajando con loops

---
+++ src/Loops/While.java
@@ -0,0 +1,26 @@
+package Loops;
+
+public class While {
+    public static void main(String[] args) {
+//        int index = 0;
+//        String [] names = {"Fredy","Gerardo", "Gutierrez"};
+//        while (index < names.length){
+//            System.out.println(names[index]);
+//            index++;
+//            if (names[index].equals("Fredy")){
+//
+//                index+=2;
+//
+//            }
+//        }
+
+
+        //do while
+
+        int index2 = 0;
+        do{
+            System.out.println("Hola, java!");
+            index2++;
+        }while (index2 < 5);
+    }
+}
