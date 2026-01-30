e7ee6c328452addf96e8135cad68ae54a075e53c
Fredy Lopez <fredygutierrez1661@gmail.com>
2026-01-29 21:05:22 -0600

    Realizando funciones sin retorno

---
+++ src/Functions/Function.java
@@ -0,0 +1,16 @@
+package Functions;
+
+public class Function {
+    public static void main(String[] args) {
+        //Funciones
+        for (int i = 0; i < 5; i++){
+            sendEmail();
+
+        }
+    }
+    //FUNCIONES SIN RETORNO
+
+    public static void sendEmail(){
+        System.out.println("Enviar email");
+    }
+}
