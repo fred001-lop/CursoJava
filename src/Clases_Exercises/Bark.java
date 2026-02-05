75d79f4f5c9f18ec7d8b6e5e81fbdab7c89948bd
Fredy Lopez <fredygutierrez1661@gmail.com>
2026-02-04 19:04:17 -0600

    Inicio de ejercicios de clases

---
+++ src/Clases_Exercises/Bark.java
@@ -0,0 +1,18 @@
+package Clases_Exercises;
+
+public class Bark {
+
+    //atributos
+    String sonido;
+
+    //constructor
+    public Bark(String sonido){
+        this.sonido = sonido;
+    }
+
+    //metodo
+
+    public  void bark(){
+        System.out.println(sonido);
+    }
+}
---
+++ src/Clases_Exercises/Exercise_2.java
@@ -0,0 +1,14 @@
+package Clases_Exercises;
+
+public class Exercise_2 {
+    public static void main(String[] args) {
+        // 2. Crea una clase Dog con un método bark() que imprima su sonido.
+
+        var dog = new Bark("Guaf");
+        dog.bark();
+
+
+        var cat = new Bark("Miau");
+        cat.bark();
+    }
+}
