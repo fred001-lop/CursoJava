dd2604ba73f8f351681559b8f30909f873e9dc09
Fredy Lopez <fredygutierrez1661@gmail.com>
2026-02-14 00:14:59 -0600

    Finalizados los ejercicios de clases

--- src/AccessModifiers/Car.java
+++ src/AccessModifiers/Car.java
@@ -5,17 +5,36 @@ public class Car {
     private int speed;
 
     //constructor
-    public Car(int speed){
-        this.speed = speed;
+    public Car(){
+        this.speed = 0;
     }
 
-    //setter
+    //metodo para acelerar
+    public void accelerate(int amount){
+        if (amount > 0){
+            speed += amount;
+            if (speed > 120){
+                speed =120;
+            }
+        }
 
-
-    //metodo
-    public int accelerate(int amount, int brake){
-        return
     }
+
+    //METODO PARA FRENAR
+    public void brake(int amount){
+        if (amount > 0){
+            speed -= amount;
+            if (speed < 0){
+                speed = 0;
+            }
+        }
+    }
+
+    //metodo para calcular velocidad actual
+    public int getSpeed(){
+        return speed;
+    }
+
 
 
 
--- src/PruebaModifiers/Exercise_10.java
+++ src/PruebaModifiers/Exercise_10.java
@@ -1,7 +1,16 @@
 package PruebaModifiers;
 
+import AccessModifiers.Car;
+
 public class Exercise_10 {
     public static void main(String[] args) {
         // 10. Crea una clase Car con el atributo privado speed. Agrega los métodos accelerate(int amount) que aumente la velocidad (máximo 120) y brake(int amount) que reduzca la velocidad (mínimo 0).
+        var car = new Car();
+        System.out.println("Velocidad inicial" + car.getSpeed());
+        car.accelerate(100);
+        System.out.println("Velocidad despues de agreagar speed " + car.getSpeed());
+        car.accelerate(50);
+        System.out.println("Velocidad despues de agreagar speed " + car.getSpeed());
+        
     }
 }
