e43be2613679bdfb9931b84079ede36297bb7139
Fredy Lopez <fredygutierrez1661@gmail.com>
2026-02-23 22:27:25 -0600

    iniciando con polimorfismo

---
+++ src/Polymorphis/Polimorfismo.java
@@ -0,0 +1,14 @@
+package Polymorphis;
+
+public class Polimorfismo {
+    public static void main(String[] args) {
+        //Polimorfismo
+        //Polimorfismo por herencia (sobrescritura)
+
+    }
+    public static class Animal{
+        public void sound(){
+            System.out.println("Algun sonido");
+        }
+    }
+}
